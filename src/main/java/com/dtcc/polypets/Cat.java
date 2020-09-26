39c08aaf5765d5d2c32e73ffb4e8fb4faa8e66af
RWLangwig <rlangwig@verizon.net>
2020-09-26 09:27:19 -0400

    New code
    Polymorphism

---
+++ src/main/java/com/dtcc/polypets/Cat.java
@@ -0,0 +1,15 @@
+package com.dtcc.polypets;
+
+public class Cat extends Pet{
+
+    public Cat(String name){
+        super(name);    }
+
+//  public Cat(String name, String speak){
+//  super(name, speak);}
+
+    public String getSpeak(){
+        String speak = "Meow!";
+        return speak;    }
+}
+
---
+++ src/main/java/com/dtcc/polypets/Snake.java
@@ -0,0 +1,16 @@
+package com.dtcc.polypets;
+
+public class Snake extends Pet{
+	
+	 public Snake(String name){
+	        super(name);	    }
+
+//	    public Snake(String name, String speak){
+//	        super(name, speak);	    }
+	 
+	    public String getSpeak(){
+	        String speak = "Hissss";
+	        return speak;	    }
+	}
+
+
